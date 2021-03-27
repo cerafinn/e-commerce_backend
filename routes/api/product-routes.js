07c66fb559396235e31df685ef783844eb46a21c
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-27 17:04:54 -0400

    working on product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,6 +5,15 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
+  Product.findAll({
+    include: [
+      Category,
+      { model: Tag,
+        through: Vote,
+        as: 'voted_posts'
+      }
+    ]
+  })
   // find all products
   // be sure to include its associated Category and Tag data
 });
