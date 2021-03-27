f6c72749c6aee1ee12119c0f1a56e0250e4f83f3
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-27 17:00:04 -0400

    set up index for models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category, as a category can have multiple products but a product can only belong to one category.
+Product.belongsTo(Category, {foreignKey: 'category_id'});
 
 // Categories have many Products
+Category.hasMany(Product, {foreignKey: 'category_id'});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag). Using the ProductTag through model, allow products to have multiple tags and tags to have many products.
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
